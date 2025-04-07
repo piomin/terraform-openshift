fb8fa084071f5c8afca6cee803f0bff9ab6cbb9d
piomin <piotr.minkowski@gmail.com>
2025-04-07 11:50:49 +0200

    RHDH(1)

--- developer-hub.sh
+++ developer-hub.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-terraform apply -auto-approve -var cluster-context=default/api-yg46z7uk-eastus-aroapp-io:6443/kube:admin -var sonar-token=${SONARQUBE_TOKEN} -var github-token=${GITHUB_TOKEN} -var argocd-token=${ARGOCD_TOKEN} -var openshift-token=${OPENSHIFT_TOKEN} -var github-client-secret=${GITHUB_CLIENT_SECRET} -var azure-token=${AZURE_TOKEN}
+terraform apply -auto-approve -var cluster-context=default/api-z4g1r69s-eastus-aroapp-io:6443/kube:admin -var sonar-token=${SONARQUBE_TOKEN} -var github-token=${GITHUB_TOKEN} -var argocd-token=${ARGOCD_TOKEN} -var openshift-token=${OPENSHIFT_TOKEN} -var github-client-secret=${GITHUB_CLIENT_SECRET} -var azure-token=${AZURE_TOKEN}
