fe69518a54dae0e665110d7606dfd2b95211d57f
piomin <piotr.minkowski@gmail.com>
2025-06-15 00:12:10 +0200

    add gpu to ai

--- ai/operators.tf
+++ ai/operators.tf
@@ -1,3 +1,263 @@
+resource "kubernetes_manifest" "nfd-group" {
+  manifest = {
+    "apiVersion" = "operators.coreos.com/v1"
+    "kind"       = "OperatorGroup"
+    "metadata"   = {
+      "name"      = "openshift-nfd"
+      "namespace" = "openshift-nfd"
+    }
+    "spec" = {
+      "upgradeStrategy" = "Default"
+      "targetNamespaces" = ["openshift-nfd"]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "nfd-operator" {
+  manifest = {
+    "apiVersion" = "operators.coreos.com/v1alpha1"
+    "kind"       = "Subscription"
+    "metadata"   = {
+      "name"      = "nfd"
+      "namespace" = "openshift-nfd"
+    }
+    "spec" = {
+      "channel"             = "stable"
+      "installPlanApproval" = "Automatic"
+      "name"                = "nfd"
+      "source"              = "redhat-operators"
+      "sourceNamespace"     = "openshift-marketplace"
+      "startingCSV"         = "nfd.4.18.0-202505200035"
+    }
+  }
+}
+
+resource "time_sleep" "wait-150-seconds-1" {
+  depends_on = [kubernetes_manifest.nfd-operator]
+
+  create_duration = "150s"
+}
+
+resource "kubernetes_manifest" "nfd-instance" {
+  depends_on = [time_sleep.wait-150-seconds-1]
+  manifest = {
+    "apiVersion" = "nfd.openshift.io/v1"
+    "kind"       = "NodeFeatureDiscovery"
+    "metadata"   = {
+      "name"      = "nfd-instance"
+      "namespace" = "openshift-nfd"
+    }
+    "spec" = {
+      "workerConfig" = {
+        "configData" = <<-EOT
+          core:
+            sleepInterval: 60s
+          sources:
+            pci:
+              deviceClassWhitelist:
+                - "0200"
+                - "03"
+                - "12"
+              deviceLabelFields:
+                - "vendor"
+        EOT
+      }
+      "operand" = {
+        "imagePullPolicy" = "IfNotPresent"
+        "servicePort"     = 12000
+      }
+      "customConfig" = {
+        "configData" = ""
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "nvidia-gpu-group" {
+  manifest = {
+    "apiVersion" = "operators.coreos.com/v1"
+    "kind"       = "OperatorGroup"
+    "metadata"   = {
+      "name"      = "nvidia-gpu-operator"
+      "namespace" = "nvidia-gpu-operator"
+    }
+    "spec" = {
+      "upgradeStrategy" = "Default"
+      "targetNamespaces" = ["nvidia-gpu-operator"]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "nvidia-gpu-operator" {
+  manifest = {
+    "apiVersion" = "operators.coreos.com/v1alpha1"
+    "kind"       = "Subscription"
+    "metadata"   = {
+      "name"      = "gpu-operator-certified"
+      "namespace" = "nvidia-gpu-operator"
+    }
+    "spec" = {
+      "channel"             = "stable"
+      "installPlanApproval" = "Automatic"
+      "name"                = "gpu-operator-certified"
+      "source"              = "certified-operators"
+      "sourceNamespace"     = "openshift-marketplace"
+      "startingCSV"         = "gpu-operator-certified.v25.3.1"
+    }
+  }
+}
+
+resource "time_sleep" "wait-150-seconds-2" {
+  depends_on = [kubernetes_manifest.nvidia-gpu-operator]
+
+  create_duration = "150s"
+}
+
+resource "kubernetes_manifest" "nvidia-gpu-cluster-policy" {
+  depends_on = [time_sleep.wait-150-seconds-2]
+  manifest = {
+    "apiVersion" = "nvidia.com/v1"
+    "kind"       = "ClusterPolicy"
+    "metadata"   = {
+      "name" = "gpu-cluster-policy"
+    }
+    "spec" = {
+      "migManager" = {
+        "enabled" = true
+        "config"  = {
+          "default" = "all-disabled"
+          "name"    = "default-mig-parted-config"
+        }
+      }
+      "operator" = {
+        "defaultRuntime"          = "crio"
+        "initContainer"           = {}
+        "runtimeClass"            = "nvidia"
+        "use_ocp_driver_toolkit"  = true
+      }
+      "dcgm" = {
+        "enabled" = true
+      }
+      "gfd" = {
+        "enabled" = true
+      }
+      "dcgmExporter" = {
+        "enabled" = true
+        "config"  = {
+          "name" = ""
+        }
+        "serviceMonitor" = {
+          "enabled" = true
+        }
+      }
+      "cdi" = {
+        "enabled" = false
+        "default" = false
+      }
+      "driver" = {
+        "enabled" = true
+        "licensingConfig" = {
+          "nlsEnabled"   = true
+          "configMapName" = ""
+        }
+        "kernelModuleType" = "auto"
+        "certConfig" = {
+          "name" = ""
+        }
+        "kernelModuleConfig" = {
+          "name" = ""
+        }
+        "upgradePolicy" = {
+          "autoUpgrade" = true
+          "drain" = {
+            "deleteEmptyDir" = false
+            "enable"         = false
+            "force"          = false
+            "timeoutSeconds" = 300
+          }
+          "maxParallelUpgrades" = 1
+          "maxUnavailable"      = "25%"
+          "podDeletion" = {
+            "deleteEmptyDir" = false
+            "force"          = false
+            "timeoutSeconds" = 300
+          }
+          "waitForCompletion" = {
+            "timeoutSeconds" = 0
+          }
+        }
+        "repoConfig" = {
+          "configMapName" = ""
+        }
+        "virtualTopology" = {
+          "config" = ""
+        }
+        "useNvidiaDriverCRD" = false
+      }
+      "devicePlugin" = {
+        "enabled" = true
+        "config"  = {
+          "name"    = ""
+          "default" = ""
+        }
+        "mps" = {
+          "root" = "/run/nvidia/mps"
+        }
+      }
+      "gdrcopy" = {
+        "enabled" = false
+      }
+      "kataManager" = {
+        "config" = {
+          "artifactsDir" = "/opt/nvidia-gpu-operator/artifacts/runtimeclasses"
+        }
+      }
+      "mig" = {
+        "strategy" = "single"
+      }
+      "sandboxDevicePlugin" = {
+        "enabled" = true
+      }
+      "validator" = {
+        "plugin" = {
+          "env" = [
+            {
+              "name"  = "WITH_WORKLOAD"
+              "value" = "false"
+            }
+          ]
+        }
+      }
+      "nodeStatusExporter" = {
+        "enabled" = true
+      }
+      "daemonsets" = {
+        "rollingUpdate" = {
+          "maxUnavailable" = "1"
+        }
+        "updateStrategy" = "RollingUpdate"
+      }
+      "sandboxWorkloads" = {
+        "defaultWorkload" = "container"
+        "enabled"         = false
+      }
+      "gds" = {
+        "enabled" = false
+      }
+      "vgpuManager" = {
+        "enabled" = false
+      }
+      "vfioManager" = {
+        "enabled" = true
+      }
+      "toolkit" = {
+        "enabled"    = true
+        "installDir" = "/usr/local/nvidia"
+      }
+    }
+  }
+}
+
 resource "kubernetes_manifest" "ossm" {
   manifest = {
     "apiVersion" = "operators.coreos.com/v1alpha1"
@@ -48,24 +308,6 @@ resource "kubernetes_manifest" "serverless" {
       "channel"             = "stable"
       "installPlanApproval" = "Automatic"
       "name"                = "serverless-operator"
-      "source"              = "redhat-operators"
-      "sourceNamespace"     = "openshift-marketplace"
-    }
-  }
-}
-
-resource "kubernetes_manifest" "pipelines" {
-  manifest = {
-    "apiVersion" = "operators.coreos.com/v1alpha1"
-    "kind"       = "Subscription"
-    "metadata" = {
-      "name"      = "openshift-pipelines-operator-rh"
-      "namespace" = "openshift-operators"
-    }
-    "spec" = {
-      "channel"             = "latest"
-      "installPlanApproval" = "Automatic"
-      "name"                = "openshift-pipelines-operator-rh"
       "source"              = "redhat-operators"
       "sourceNamespace"     = "openshift-marketplace"
     }
